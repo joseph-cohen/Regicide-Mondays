bc7f88a5baeff62efe571271a5aa9355b1d5aacb
Joseph Cohen <jcohend@atid.edu.mx>
2019-01-09 17:32:49 -0600

    Added Days

---
+++ Regicide Mondays/Assets/Scripts/DaySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaySpawner : MonoBehaviour
+{
+
+    public GameObject day;
+
+    void SpawnDay(Vector2 pos) {
+        Object.Instantiate(day, pos, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < 7; i++) {
+            
+            Vector2 posi = new Vector2((i - 6) * 1 , 0);
+
+            SpawnDay(posi);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
